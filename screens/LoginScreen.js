0b0af831a30b65a8189d34aea6eaf754addc97c5
Dev singh pawar <pawarsinghdev@gmail.com>
2024-05-19 03:31:54 +0530

    Added Login function

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/Ui/LoadingOverlay";
 import { login } from "../util/auth";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await login(email, password);
-    setIsAuthenticating(false);
+    try {
+      const token = await login(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not log you in. Please check your credentials or try again later!"
+      );
+      setIsAuthenticating(false);
+    }
   }
 
   if (isAuthenticating) {
